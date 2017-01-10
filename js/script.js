0dd44db0c7fd8dfcc9bb45ebac528494dd32162d
Extreeeme <martindcontact@gmail.com>
2017-01-10 10:55:01 +0100

    Ajout de l'image

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-var nbCase = prompt("Entrez un nombre de case", "");
+var nbCase = prompt("Entrez un nombre de case");
 
-if(nbCase < 800){
+if(nbCase < 800 && nbCase == parseInt(nbCase)){
 
 	for (var i = 1; i <=nbCase; i++) {
 
     document.getElementById('liste').innerHTML += "<li>"+i+"</li>";
-    
+
     }
+}
+
+else{
+	document.getElementById('liste').innerHTML += "<img src='img/doigt.gif'></img>";
 }
